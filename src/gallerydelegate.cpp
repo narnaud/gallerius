84644d7059e7b48fde283029c0d5b7479b3ae481
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2021-01-10 19:27:06 +0100

    Ensure text is not written on top of the image

--- src/gallerydelegate.cpp
+++ src/gallerydelegate.cpp
@@ -5,6 +5,8 @@
 #include <QPainter>
 #include <QStyleOptionViewItem>
 
+static constexpr int TextMargin = 18;
+
 GalleryDelegate::GalleryDelegate(QObject *parent)
     : QItemDelegate(parent)
 {
@@ -18,21 +20,23 @@ void GalleryDelegate::paint(QPainter *painter, const QStyleOptionViewItem &optio
     const QString &text = index.data().toString();
     const QPixmap &pixmap = index.data(Qt::DecorationRole).value<QPixmap>();
 
-    const QColor color = filter
-        ? (selected ? QColor(Qt::magenta).lighter() : QColor(Qt::red).lighter())
-        : (selected ? QColor(Qt::blue).lighter() : Qt::lightGray);
+    QColor color = filter ? (selected ? QColor(Qt::magenta).lighter() : QColor(Qt::red).lighter())
+                          : (selected ? QColor(Qt::blue).lighter() : Qt::lightGray);
     painter->fillRect(option.rect, color);
 
     if (!pixmap.isNull()) {
         const int hMargin = (option.rect.width() - pixmap.width()) / 2;
-        const int vMargin = (option.rect.height() - pixmap.height()) / 2;
-        const QRect rect = option.rect.adjusted(hMargin, vMargin, -hMargin, -vMargin);
+        const int vMargin = (option.rect.height() - pixmap.height() - TextMargin + Margin) / 2;
+        const QRect rect = option.rect.adjusted(hMargin, vMargin, -hMargin, -vMargin - TextMargin);
         ;
         painter->drawPixmap(rect, pixmap);
     }
 
-    const QRect textRect = {option.rect.left(), option.rect.bottom() - 20, option.rect.width(), 20};
-    painter->drawText(textRect, Qt::AlignCenter, text);
+    const QRect textRect = {option.rect.left(), option.rect.bottom() - TextMargin,
+                            option.rect.width(), TextMargin};
+    color.setAlpha(128);
+    painter->fillRect(textRect, color);
+    painter->drawText(textRect, Qt::AlignHCenter | Qt::AlignTop, text);
 }
 
 QSize GalleryDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
@@ -40,5 +44,5 @@ QSize GalleryDelegate::sizeHint(const QStyleOptionViewItem &option, const QModel
     Q_UNUSED(option)
     Q_UNUSED(index)
     return QSize(GalleryModel::ThumbnailSize + 2 * Margin,
-                 GalleryModel::ThumbnailSize + 2 * Margin);
+                 GalleryModel::ThumbnailSize + Margin + TextMargin);
 }
